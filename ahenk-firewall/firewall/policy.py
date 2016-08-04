cc07791969b97b9333eca4a6adf67399cad29dfc
mine.dogan <mine.dogan@agem.com.tr>
2016-08-04 12:01:49 +0300

    firewall ahenk side

--- ahenk-firewall/firewall/policy.py
+++ ahenk-firewall/firewall/policy.py
@@ -1,23 +1,57 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# Author:Mine DOGAN <mine.dogan@agem.com.tr>
+
+import json
 
 from base.plugin.abstract_plugin import AbstractPlugin
 
 
-class Sample(AbstractPlugin):
+class FirewallRules(AbstractPlugin):
     def __init__(self, profile_data, context):
-        super(Sample, self).__init__()
+        super(FirewallRules, self).__init__()
         self.profile_data = profile_data
         self.context = context
         self.logger = self.get_logger()
+        self.message_code = self.get_message_code()
+
+        self.parameters = json.loads(self.profile_data)
+
+        self.rules = self.parameters['rules']
+        self.temp_file_name = str(self.generate_uuid())
+        self.file_path = '{0}{1}'.format(str(self.Ahenk.received_dir_path()), self.temp_file_name)
+        self.initial_rules_file_path = '/etc/ahenk/iptables.rules'
+
+        self.logger.debug('[FIREWALL] Parameters were initialized.')
 
     def handle_policy(self):
-        # TODO Do what do you want to do!
-        # TODO Don't Forget returning response with <self.context.create_response(..)>
-        pass
+        try:
+            self.logger.debug('[FIREWALL] Export initial rules to a temporary file...')
+            self.execute('/sbin/iptables-save > {}'.format(self.initial_rules_file_path))
+
+            self.logger.debug('[FIREWALL] Writing rules to temporary file...')
+            self.write_file(self.file_path, '{0}{1}'.format(self.rules, '\n'))
+
+            self.logger.debug('[FIREWALL] Adding temp file to iptables-restore as parameter...')
+            self.execute('/sbin/iptables-restore < {}'.format(self.file_path))
+
+            self.logger.debug('[FIREWALL] Save the rules...')
+            self.execute('service netfilter-persistent save')
+
+            self.logger.debug('[FIREWALL] Restart the service...')
+            self.execute('service netfilter-persistent restart')
+
+            self.context.create_response(code=self.message_code.POLICY_PROCESSED.value,
+                                         message='Güvenlik Duvarı kuralları başarıyla kaydedildi.')
+            self.logger.info('[FIREWALL] Firewall policy is handled successfully')
+
+        except Exception as e:
+            self.logger.error(
+                '[FIREWALL] A problem occured while handling Firewall policy: {0}'.format(str(e)))
+            self.context.create_response(code=self.message_code.POLICY_ERROR.value,
+                                         message='Güvenlik Duvarı profili uygulanırken bir hata oluştu.')
 
 
 def handle_policy(profile_data, context):
-    print('Sample Plugin Policy')
-    sample = Sample(profile_data, context)
-    sample.handle_policy()
+    set_rules = FirewallRules(profile_data, context)
+    set_rules.handle_policy()
--- ahenk-firewall/firewall/safe.py
+++ ahenk-firewall/firewall/safe.py
@@ -11,9 +11,19 @@ class Safe(AbstractPlugin):
         self.username = str(context.get_username())
         self.logger = self.get_logger()
 
+        self.initial_rules_file_path = '/etc/ahenk/iptables.rules'
+        self.logger.debug('[FIREWALL - safe] Parameters were initialized.')
+
+
     def handle_safe_mode(self):
-        # TODO Do what do you want to do!
-        pass
+        self.logger.debug('[FIREWALL - safe] Adding initial rules temp file to iptables-restore as parameter...')
+        self.execute('/sbin/iptables-restore < {}'.format(self.initial_rules_file_path))
+
+        self.logger.debug('[FIREWALL - safe] Save the rules...')
+        self.execute('service netfilter-persistent save')
+
+        self.logger.debug('[FIREWALL - safe] Restart the service...')
+        self.execute('service netfilter-persistent restart')
 
 
 def handle_mode(context):
